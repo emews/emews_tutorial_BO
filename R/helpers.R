e703b2c03cee398b4aec8ebffa21cce1c6ab0eb5
abbystvns <abby.e.stevens@gmail.com>
2024-12-16 15:29:09 -0800

    R knit

--- R/helpers.R
+++ R/helpers.R
@@ -1,10 +1,11 @@
 library(ggplot2)
 library(dplyr)
 library(reshape2)
-library(MASS) # for mvrnorm
+library(MASS) 
 library(yaml)
 library(jsonlite)
 
+# Scale matrix within specified bounds
 to_native <- function(x, lb = c(0.1, 1), ub = c(1, 3)) {
   x_native <- matrix(NA, nrow = nrow(x), ncol = ncol(x))
   for (i in seq_len(ncol(x))) {
@@ -13,6 +14,7 @@ to_native <- function(x, lb = c(0.1, 1), ub = c(1, 3)) {
   return(x_native)
 }
 
+# Generate inputs in required format for EMEWS
 generate_inputs <- function(Xgrid, ids, seed_tracker, seeds, lb = c(0.1, 1), ub = c(1, 3)) {
   Xgrid_native <- to_native(Xgrid, lb = lb, ub = ub)
   Xs <- list()
@@ -33,48 +35,8 @@ generate_inputs <- function(Xgrid, ids, seed_tracker, seeds, lb = c(0.1, 1), ub 
   return(list(X = X[, 1:2], Xnative = Xnative))
 }
 
-# Define `run_reps`
-run_reps <- function(ids, seeds) {
-  X <- list()
-  Y <- list()
-  
-  for (s in seeds) {
-    X <- append(X, list(Xgrid[ids, ]))
-    Y <- append(Y, list(Ygrid[ids, s]))
-  }
-  
-  X <- do.call(rbind, X)
-  Y <- unlist(Y)
-  return(list(X = X, Y = Y))
-}
 
-
-scale_y <- function(Y) {
-  Y_scaled <- (Y - mean(Y)) / sd(Y)
-  return(list(Y_scaled = Y_scaled, stats = c(mean = mean(Y), sd = sd(Y))))
-}
-
-initial_samples <- function(n = 5, grid_size = 30, init_id = NULL) {
-  xx <- seq(0, 1, length.out = grid_size)
-  Xgrid <- expand.grid(xx, xx)
-  
-  if (is.null(init_id)) {
-    N <- nrow(Xgrid)
-    init_id <- sample(1:N, n, replace = FALSE)
-  }
-  
-  return(list(Xinit = Xgrid[init_id, ], Xgrid = Xgrid))
-}
-
-to_01 <- function(X) {
-  min_vals <- apply(X, 2, min)
-  max_vals <- apply(X, 2, max)
-  scale <- max_vals - min_vals
-  X_scaled <- sweep(sweep(X, 2, min_vals, "-"), 2, scale, "/")
-  return(X_scaled)
-}
-
-
+# Thomspon sample
 TS_npoints <- function(model, npoints, Xgrid) {
   preds <- predict(model, Xgrid, xprime=Xgrid)
   pred_mean <- preds$mean
@@ -86,7 +48,8 @@ TS_npoints <- function(model, npoints, Xgrid) {
 }
 
 
-plot_gp_mean <- function(model, Xgrid, X, ymean = 0, ystd = 1, logged = TRUE, title = NULL) {
+# Plot mean surface/variance
+plot_gp <- function(model, Xgrid, X, ymean = 0, ystd = 1, title = NULL) {
   Xgrid_native <- to_native(Xgrid)
   X_native <- to_native(X)
   
@@ -96,16 +59,29 @@ plot_gp_mean <- function(model, Xgrid, X, ymean = 0, ystd = 1, logged = TRUE, ti
   preds <- predict(model, Xgrid, xprime=Xgrid)
   pred_mean <- preds$mean * ystd + ymean
   
-  if (logged) {
-    df$surface <- exp(pred_mean)
-  }
-  
-  p <- ggplot() +
-    geom_tile(data = df, aes(x = zombie_step_size, y = human_step_size, fill = surface)) +
-    geom_point(data = as.data.frame(X_native), aes(x = V1, y = V2), color = "black", alpha=.1, shape=1, size=3) +
+  df$Std <- sqrt(preds$sd2)
+  df$`Surviving Humans` <- exp(pred_mean)
+
+  p1 <- ggplot() +
+    geom_tile(data = df, aes(x = zombie_step_size, y = human_step_size, fill = `Surviving Humans`)) +
+    geom_point(data = as.data.frame(X_native), aes(x = V1, y = V2), color = "black", alpha=.2, shape=1, size=3) +
     scale_fill_gradient(low = "lightblue", high = "cornflowerblue") +
-    labs(title = title, x = "Zombie Step Size", y = "Human Step Size") +
-    theme_minimal()
+    labs(title = 'Mean Surface', x = "Zombie Step Size", y = "Human Step Size", fill=NULL) +
+    theme_minimal() + 
+    theme(legend.box.spacing = unit(0.1, "lines"),
+          legend.margin = margin(0, 0, 0, 0),
+          plot.title = element_text(hjust = 0.5))
   
-  return(p)
+  p2 <- ggplot() +
+    geom_tile(data = df, aes(x = zombie_step_size, y = human_step_size, fill = Std)) +
+    geom_point(data = as.data.frame(X_native), aes(x = V1, y = V2), color = "black", alpha=.2, shape=1, size=3) +
+    scale_fill_gradient(low = "#E0FFE0", high = "seagreen") +
+    labs(title = 'Variance', x = "Zombie Step Size", y = "Human Step Size", fill=NULL) +
+    theme_minimal() + 
+    theme(legend.box.spacing = unit(0.1, "lines"),
+          legend.margin = margin(0, 0, 0, 0),
+          plot.title = element_text(hjust = 0.5))
+  
+  plots <- list(mean_surface=p1, pred_var=p2)
+  return(plots)
 }
